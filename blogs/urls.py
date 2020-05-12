9bfa07d0a48f32fb879f4c768415f32b41bc608e
sepehr javid <javid.sepehr77@gmail.com>
2020-05-12 13:50:49 +0430

    add static and media url

--- blogs/urls.py
+++ blogs/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+
+from blogs import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login', obtain_auth_token),
     path('api/posts/', include('posts.urls', namespace='posts'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
